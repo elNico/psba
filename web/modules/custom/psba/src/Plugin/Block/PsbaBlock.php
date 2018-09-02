172c1a1031ad6df47288e93e4dbb15ae18b30c25
elNico <nicolas.haase@gmail.com>
2018-09-02 21:30:56 +1000

    PSBA block config field.

--- web/modules/custom/psba/src/Plugin/Block/PsbaBlock.php
+++ web/modules/custom/psba/src/Plugin/Block/PsbaBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\psba\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a PSBA Block.
@@ -13,7 +15,7 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("PSBA"),
  * )
  */
-class PsbaBlock extends BlockBase {
+class PsbaBlock extends BlockBase implements BlockPluginInterface {
 
   /**
    * {@inheritdoc}
@@ -22,6 +24,24 @@ class PsbaBlock extends BlockBase {
     return array(
       '#markup' => $this->t('Custom content.'),
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['psba_block_custom'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Custom value'),
+      '#description' => $this->t('An example of saving custom configuration values.'),
+      '#default_value' => isset($config['psba_block_custom']) ? $config['psba_block_custom'] : '',
+    ];
+
+    return $form;
   }
 
 }
